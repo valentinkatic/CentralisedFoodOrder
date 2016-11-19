f08431533b633b9ba172a377e67916142bad2957
Valentin Katić <valentin.katic@gmail.com>
2016-11-19 16:34:21 +0100

    kreacija TabHost-a s 2 taba

--- app/src/main/java/com/katic/centralisedfoodorder/ChooseActivity.java
+++ app/src/main/java/com/katic/centralisedfoodorder/ChooseActivity.java
@@ -5,14 +5,18 @@ import com.katic.centralisedfoodorder.adapter.RVAdapter;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
+import android.widget.TabHost;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -22,19 +26,44 @@ public class ChooseActivity extends Activity {
 
     private List<Restaurant> restaurants;
     private RecyclerView rv;
+    private RecyclerView rv2;
+    TabHost tabHost;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose);
 
+        TabHost host = (TabHost)findViewById(R.id.tabHost);
+        host.setup();
+
+        //Tab 1
+        TabHost.TabSpec spec = host.newTabSpec("All");
+        spec.setContent(R.id.tab1);
+        spec.setIndicator("All");
+        host.addTab(spec);
+
+        //Tab 2
+        spec = host.newTabSpec("Bookmarks");
+        spec.setContent(R.id.tab2);
+        spec.setIndicator("Bookmarks");
+        host.addTab(spec);
+
         HorizontalListView listview = (HorizontalListView) findViewById(R.id.listview);
         listview.setAdapter(new HAdapter());
+
+        HorizontalListView listview2 = (HorizontalListView) findViewById(R.id.listview2);
+        listview2.setAdapter(new HAdapter());
 
         rv = (RecyclerView) findViewById(R.id.restaurantList);
         LinearLayoutManager llm = new LinearLayoutManager(this);
         rv.setLayoutManager(llm);
         rv.setHasFixedSize(true);
+
+        rv2 = (RecyclerView) findViewById(R.id.restaurantList2);
+        LinearLayoutManager llm2 = new LinearLayoutManager(this);
+        rv2.setLayoutManager(llm2);
+        rv2.setHasFixedSize(true);
 
         initializeData();
         initializeAdapter();
@@ -51,6 +80,7 @@ public class ChooseActivity extends Activity {
     private void initializeAdapter(){
         RVAdapter adapter = new RVAdapter(restaurants);
         rv.setAdapter(adapter);
+        rv2.setAdapter(adapter);
     }
 
     private static String[] dataObjects = new String[]{
